951510eae4d7e67ab8b96b5a05f7a6825a29dd84
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2025-01-22 01:17:01 +0530

    Update hero.tsx

--- src/components/app/hero.tsx
+++ src/components/app/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     return (
         <main className="p-4 flex flex-col mt-24">
             <h1 className="text-5xl font-syne font-bold">Aditya Kanu</h1>
-            <p className="font-mont">Aspiring <code>Software Engineer</code> and <code>Embedded Software Engineer</code>. Currently pursuing my <span className="font-medium ">B.Tech</span> in <span className="font-medium ">Electronics and Instrumentation Engineering</span> at the <span className="font-medium ">National Institute of Technology Agartala</span>.</p>
+            <p className="font-mont"><code>Software Development Intern</code> at <code>Zscaler</code>. Pursuing my <span className="font-medium ">B.Tech</span> in <span className="font-medium ">Electronics and Instrumentation Engineering</span> at the <span className="font-medium ">National Institute of Technology Agartala</span>.</p>
         </main>
 
     );
